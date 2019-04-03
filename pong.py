5482317e58f19d1e83d66f62dc0c5d316eb6107a
gibekk <kamilgibowski90@gmail.com>
2019-04-03 20:28:21 +0200

    Running window loop

--- pong.py
+++ pong.py
@@ -3,3 +3,10 @@ import pygame
 (width, height) = (300, 200)
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
+
+running = True
+while running:
+    eventqueue = pygame.event.get()
+    for event in eventqueue:
+        if event.type == pygame.QUIT:
+            running = False
