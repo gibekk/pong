a40abad1c9ad3018e672ddd6a70cbe0a52b5e612
gibekk <kamilgibowski90@gmail.com>
2019-04-03 21:01:47 +0200

    Swapping window buffer

--- pong.py
+++ pong.py
@@ -2,13 +2,13 @@ import pygame
 
 (width, height) = (300, 200)
 screen = pygame.display.set_mode((width, height))
-pygame.display.flip()
 pygame.display.set_caption('Tutorial 1')
 background_colour = (13, 154, 50)
 screen.fill(background_colour)
 
 running = True
 while running:
+    pygame.display.flip()
     eventqueue = pygame.event.get()
     for event in eventqueue:
         if event.type == pygame.QUIT:
