b2084de8b2482952f2eb7ad3ed890637b8d2f452
gibekk <kamilgibowski90@gmail.com>
2019-04-06 21:13:05 +0200

    First peaddle

--- pong.py
+++ pong.py
@@ -1,35 +1,44 @@
 import pygame
+
 pygame.init()
 
-x = 50
-y = 50
-vel = 5
-width = 40
-height = 60
-screen = pygame.display.set_mode((500, 500))
+(WindowWidth, WindowHeight) = (800, 600)
+vel = 2.7
+width = WindowWidth * 0.313
+height = WindowHeight * 0.042
+x = (WindowWidth / 2) - (width / 2)
+y = 550
+
+
+
+screen = pygame.display.set_mode((WindowWidth, WindowHeight))
 pygame.display.set_caption('Tutorial 1')
 
 running = True
 while running:
-    pygame.time.delay(100)
+    pygame.time.delay(16)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT]:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+        if x < 0:
+            x = 0
+
+    elif keys[pygame.K_RIGHT]:
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+        if x > 800 - width:
+            x = 800 - width
+
+
 
     screen.fill((64, 189, 26))
-    pygame.draw.circle(screen, (255, 0, 0), (x, y), 10 )
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
     pygame.display.flip()
 
 pygame.quit()
