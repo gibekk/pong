a8f65c14ecfd87c02773f451167f14ce4d984a8b
gibekk <kamilgibowski90@gmail.com>
2019-04-06 21:29:24 +0200

    Second paddle

--- pong.py
+++ pong.py
@@ -3,13 +3,13 @@ import pygame
 pygame.init()
 
 (WindowWidth, WindowHeight) = (800, 600)
-vel = 2.7
+vel = 3
 width = WindowWidth * 0.313
 height = WindowHeight * 0.042
-x = (WindowWidth / 2) - (width / 2)
-y = 550
-
-
+x1 = (WindowWidth / 2) - (width / 2)
+y1 = 550
+x2 = (WindowWidth / 2) - (width / 2)
+y2 = 50 - height
 
 screen = pygame.display.set_mode((WindowWidth, WindowHeight))
 pygame.display.set_caption('Tutorial 1')
@@ -22,23 +22,31 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
     keys = pygame.key.get_pressed()
 
+    if keys[pygame.K_a]:
+        x2 -= vel
+        if x2 < 0:
+            x2 = 0
+
+    elif keys[pygame.K_d]:
+        x2 += vel
+        if x2 > 800 - width:
+            x2 = 800 - width
+
     if keys[pygame.K_LEFT]:
-        x -= vel
-        if x < 0:
-            x = 0
+        x1 -= vel
+        if x1 < 0:
+            x1 = 0
 
     elif keys[pygame.K_RIGHT]:
-        x += vel
-        if x > 800 - width:
-            x = 800 - width
-
-
+        x1 += vel
+        if x1 > 800 - width:
+            x1 = 800 - width
 
     screen.fill((64, 189, 26))
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
+    pygame.draw.rect(screen, (255, 0, 0), (x1, y1, width, height))
+    pygame.draw.rect(screen, (255, 0, 0), (x2, y2, width, height))
     pygame.display.flip()
 
 pygame.quit()
