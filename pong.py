e4e6332982756de198aaeb51ab5e49a95cfcefd4
gibekk <kamilgibowski90@gmail.com>
2019-04-06 17:15:07 +0200

    keyboard input

--- pong.py
+++ pong.py
@@ -1,15 +1,35 @@
 import pygame
+pygame.init()
 
-(width, height) = (300, 200)
-screen = pygame.display.set_mode((width, height))
+x = 50
+y = 50
+vel = 5
+width = 40
+height = 60
+screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Tutorial 1')
-background_colour = (13, 154, 50)
-screen.fill(background_colour)
 
 running = True
 while running:
-    pygame.display.flip()
-    eventqueue = pygame.event.get()
-    for event in eventqueue:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    screen.fill((64, 189, 26))
+    pygame.draw.circle(screen, (255, 0, 0), (x, y), 10 )
+    pygame.display.flip()
+
+pygame.quit()
