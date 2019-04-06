7d5b647760aa3b119e2f389462f489554f7d0abe
gibekk <kamilgibowski90@gmail.com>
2019-04-06 23:12:04 +0200

    Ping Pong 1.0

--- pong.py
+++ pong.py
@@ -4,6 +4,8 @@ pygame.init()
 
 (WindowWidth, WindowHeight) = (800, 600)
 vel = 3
+vel_ballx = 3
+vel_bally = 3
 width = WindowWidth * 0.313
 height = WindowHeight * 0.042
 x1 = (WindowWidth / 2) - (width / 2)
@@ -24,7 +26,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
     keys = pygame.key.get_pressed()
+    y_ball += vel_bally
+    x_ball += vel_ballx
+    if x_ball > WindowWidth or x_ball < 0:
+        vel_ballx = -vel_ballx
+
+    if y_ball >= y1 and x_ball >= x1 and x_ball <= x1 + width:
+        vel_bally = vel_bally * -1
+
+    if y_ball <= y2 + height and x_ball >= x2 and x_ball <= x2 + width:
+        vel_bally = vel_bally * -1
+
+    if y_ball < 0:
+        vel_bally = -vel_bally
 
 
     if keys[pygame.K_a]:
